eaf26e0e2c8171ba35587d06449e297ea0722ab1
Dmytrii Puzyr <slymahel@gmail.com>
2020-03-22 12:18:42 +0200

    Add theme, fonts and global styles

--- client/src/containers/app/index.tsx
+++ client/src/containers/app/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
+
+import { theme } from 'configs/theme'
+import { GlobalStyles } from 'styles/global'
 
 export const App = () => {
-  return <div>Hello to all the cooks in the world!</div>
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+
+      <div>Hello to all the cooks in the world!</div>
+    </ThemeProvider>
+  )
 }
