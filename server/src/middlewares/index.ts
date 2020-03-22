87e518a3b96572b5b0b115d91a59f96676c4df44
Dmytrii Puzyr <slymahel@gmail.com>
2020-03-22 17:01:42 +0200

    Add CORS middleware to server

--- server/src/middlewares/index.ts
+++ server/src/middlewares/index.ts
@@ -1,9 +1,12 @@
 import express, { Express } from 'express'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
+import cors from 'cors'
 
 export const initializeMiddlewares = (app: Express) => {
   app.use(logger('dev'))
+
+  app.use(cors())
 
   app.use(cookieParser())
   app.use(express.json())
